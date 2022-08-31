afd93c81b757508174ee1a5eb91f1d936baf12d0
Pablo Lugo <pablolura@unisabana.edu.co>
2022-08-31 18:40:13 -0500

    Update SUMA.java

--- MATH/src/main/java/SUMA.java
+++ MATH/src/main/java/SUMA.java
@@ -1,24 +1,26 @@
 import java.util.Scanner;
 
-
-public class SUMA {
-
+public class suma {
     Scanner reader = new Scanner(System.in);
+
     int numero1 = 0;
     int numero2 = 0;
 
-    int resultado = numero1 + numero2;
+    int resultado = numero1+numero2;
 
-    int suma() {
+    int suma(){
+
 
         System.out.println("Introduce el primer número:");
         numero1 = reader.nextInt();
 
         System.out.println("Introduce el segundo número:");
         numero2 = reader.nextInt();
+
         System.out.println("La suma es " + numero1 + " + " + numero2 + " = " + resultado);
 
-
         return 0;
+
     }
+
 }
