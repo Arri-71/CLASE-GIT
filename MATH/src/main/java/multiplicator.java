16416c780ecff4eebf18070d801d7112f68ef25f
Nicolasgosu <nicolasgosu@unisabana.edu.co>
2022-09-01 17:29:28 -0300

    multiplication
    multiplicator class and multiplication functions added

---
+++ MATH/src/main/java/multiplicator.java
@@ -0,0 +1,8 @@
+public class multiplicator {
+    public int multiplication(int num1, int num2){
+        return num1*num2;
+    }
+    public double multiplication(double num1, double num2){
+        return num1*num2;
+    }
+}
