d508ab69bb41b17a5c9fc0c444aaa5018b7705a1
Nicolasgosu <nicolasgosu@unisabana.edu.co>
2022-09-01 17:35:13 -0300

    scaner added
    added scaner to multiplicator class

--- MATH/src/main/java/multiplicator.java
+++ MATH/src/main/java/multiplicator.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 public class multiplicator {
-    public int multiplication(int num1, int num2){
-        return num1*num2;
-    }
-    public double multiplication(double num1, double num2){
+    public int multiplication(){
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Introduce el primer número:");
+        int num1 = reader.nextInt();
+
+        System.out.println("Introduce el segundo número:");
+        int num2 = reader.nextInt();
+        
+        reader.close();;
+
         return num1*num2;
     }
 }
